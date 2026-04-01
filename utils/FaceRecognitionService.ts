a955b2afee7fed7f93a44323dff58babe039f26a
Miguel <miguelrhuan30@gmail.com>
2026-04-01 19:26:20 -0300

    ajuste almoço

--- utils/FaceRecognitionService.ts
+++
@@ -1,9 +0,0 @@
-// This service is deprecated and has been replaced by Google Gemini AI.
-export const faceService = {
-  loadModels: async () => {},
-  extractFaceDescriptor: async () => null,
-  compareFaces: () => 1,
-  descriptorToString: () => "",
-  stringToDescriptor: () => new Float32Array(),
-  isMatch: () => false
-};
